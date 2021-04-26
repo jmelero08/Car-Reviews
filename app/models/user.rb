ea55ffaeefe70c4c0091c5bd203934e8b7548af9
Jose Melero Martinez <jmelero08@gmail.com>
2021-04-26 14:21:50 -0600

    Users can signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_many :reviews
     has_many :reviewed_vehicles, through: :reviews, source: :vehicle
     has_many :vehicles #they created
+    validates :username, uniqueness: true, presence: true
+    has_secure_password
 end
