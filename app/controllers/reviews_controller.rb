f0a3de4a7554902dd2fc7b4cd0e979fad84de33e
Jose Melero Martinez <jmelero08@gmail.com>
2021-05-04 13:41:19 -0600

    nested review routes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,37 @@
 class ReviewsController < ApplicationController
+
+    def new
+        if @vehicle = Vehicle.find_by_id(params[:vehicle_id])
+            @review = @vehicle.reviews.build
+        else 
+            @reviews = Review.new
+        end
+    end
+
+    def create
+        @review = current_user.reviews.build(review_params)
+        if @review.save
+            redirect_to review_path(@review)
+        else 
+            render :new
+        end
+    end
+
+    def show
+        @review = Review.find_by_id(params[:id])
+    end
+
+    def index
+        if @vehicle = Vehicle.find_by_id(params[:vehicle_id])
+            @reviews = @vehicle.reviews 
+        else
+            @reviews = Review.all
+        end
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:vehicle_id, :rating, :content)
+    end
 end
